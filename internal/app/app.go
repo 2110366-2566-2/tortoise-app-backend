c7694f9d5a99175554344a57fd043952926260ed
arrmph <arrmph@gmail.com>
2024-02-03 14:12:25 -0500

    Add login-endpoint

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/2110366-2566-2/tortoise-app-backend/internal/transport/rest"
+	"github.com/2110366-2566-2/tortoise-app-backend/internal/transport/rest/apiV1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +10,8 @@ func Run() {
 	r := gin.Default()
 
 	rest.SetupRoutes(r)
+
+	apiV1.SetupRoutes(r)
 
 	r.Run(":8080")
 }
