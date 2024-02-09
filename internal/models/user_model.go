b4ebe3abbccb0ec2e8152d3b5121d2e894ffaa12
Phuvanach  Phoemphol <arm@Arm.local>
2024-02-10 02:39:09 +0700

    Edit user struct

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -2,22 +2,23 @@ package models
 
 // user model from user collection
 type User struct {
-	User_id     string `json:"user_id"`
-	Name        string `json:"name"`
-	Surname     string `json:"surname"`
-	Gender      string `json:"gender"`
-	PhoneNumber string `json:"phone_number"`
-	Image       string `json:"image"`
-	Role        int    `json:"role"`
+	ID          string `json:"_id"`
+	Username    string `json:"username"`
 	Email       string `json:"email"`
 	Password    string `json:"password"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Gender      string `json:"gender"`
+	PhoneNumber string `json:"phoneNumber"`
+	Image       string `json:"image"`
+	Role        int    `json:"role"`
 	Address     struct {
 		Province    string `json:"province"`
 		District    string `json:"district"`
-		SubDistrict string `json:"sub_district"`
-		PostalCode  string `json:"postal_code"`
+		SubDistrict string `json:"subdistrict"`
+		PostalCode  string `json:"postalCode"`
 		Street      string `json:"street"`
 		Building    string `json:"building"`
-		HouseNumber string `json:"house_number"`
+		HouseNumber string `json:"houseNumber"`
 	} `json:"address"`
 }
