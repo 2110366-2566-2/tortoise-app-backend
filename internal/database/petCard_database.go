4e38de8f06816fc038a955abd11e16a5897f453d
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-02-12 21:34:16 +0700

    Fix bug in get all pet

--- internal/database/petCard_database.go
+++ internal/database/petCard_database.go
@@ -33,7 +33,7 @@ func (h *Handler) GetAllPetCards(ctx context.Context) (*[]models.PetCard, error)
 				{Key: "_id", Value: 1},
 				{Key: "name", Value: 1},
 				{Key: "category", Value: 1},
-				{Key: "type", Value: 1},
+				{Key: "species", Value: 1},
 				{Key: "price", Value: 1},
 				{Key: "media", Value: 1},
 				{Key: "seller_id", Value: 1},
