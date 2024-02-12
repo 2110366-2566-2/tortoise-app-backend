f35b3c373778249f9e86057817326bfa1b9131e7
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-02-12 20:36:01 +0700

    Edit pet struct

--- internal/models/pet_model.go
+++ internal/models/pet_model.go
@@ -4,24 +4,26 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // pet model from pet collection
 type Pet struct {
-	ID             primitive.ObjectID `json:"id" bson:"_id"`
-	Name           string             `json:"name"`
-	Age            int32              `json:"age"`
-	Price          int32              `json:"price"`
-	Is_sold        bool               `json:"is_sold"`
-	Description    string             `json:"description"`
-	Weight         float64            `json:"weight"`
-	Sex            string             `json:"sex"`
-	Species        string             `json:"species"`
-	Type           string             `json:"type"`
-	Behavior       string             `json:"behavior"`
-	Media          string             `json:"media"`
-	Medical_record struct {
-		Medical_id  string `json:"medical_id"`
-		Date        string `json:"date"`
-		Description string `json:"description"`
-	} `json:"medical_record"`
-	Seller_id primitive.ObjectID `json:"seller_id,omitempty"`
+	ID              primitive.ObjectID `json:"id" bson:"_id"`
+	Name            string             `json:"name"`
+	Age             int32              `json:"age"`
+	Price           int32              `json:"price"`
+	Is_sold         bool               `json:"is_sold"`
+	Description     string             `json:"description"`
+	Weight          float64            `json:"weight"`
+	Sex             string             `json:"sex"`
+	Species         string             `json:"species"`
+	Category        string             `json:"category"`
+	Behavior        string             `json:"behavior"`
+	Media           string             `json:"media"`
+	Medical_records []Medical_record   `json:"medical_records"`
+	Seller_id       primitive.ObjectID `json:"seller_id,omitempty"`
+}
+
+type Medical_record struct {
+	Medical_id   string `json:"medical_id"`
+	Medical_date string `json:"medical_date"`
+	Description  string `json:"description"`
 }
 
 // pet card model from pet collection
